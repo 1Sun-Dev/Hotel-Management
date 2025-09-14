7183906a7b11500a9c7b12a9b60a41adfde2a9d6
1Sun <truongno1st123@gmail.com>
2025-09-14 22:13:13 +0700

    Initial commit

--- Hotel Management/Form1.Designer.cs
+++ Hotel Management/Form1.Designer.cs
@@ -29,92 +29,95 @@
         private void InitializeComponent()
         {
             this.LBDangnhap = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtNameLogin = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblNameLogin = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.bttLogin = new System.Windows.Forms.Button();
+            this.bttExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LBDangnhap
             // 
-            this.LBDangnhap.AutoSize = true;
-            this.LBDangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBDangnhap.Location = new System.Drawing.Point(279, 41);
+            this.LBDangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBDangnhap.Location = new System.Drawing.Point(273, 38);
             this.LBDangnhap.Name = "LBDangnhap";
-            this.LBDangnhap.Size = new System.Drawing.Size(112, 24);
+            this.LBDangnhap.Size = new System.Drawing.Size(158, 44);
             this.LBDangnhap.TabIndex = 0;
             this.LBDangnhap.Text = "Đăng nhập";
             this.LBDangnhap.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // txtNameLogin
             // 
-            this.textBox1.Location = new System.Drawing.Point(230, 137);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(183, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtNameLogin.Location = new System.Drawing.Point(320, 130);
+            this.txtNameLogin.Name = "txtNameLogin";
+            this.txtNameLogin.Size = new System.Drawing.Size(183, 20);
+            this.txtNameLogin.TabIndex = 1;
             // 
-            // textBox2
+            // txtPassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(230, 202);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(183, 20);
-            this.textBox2.TabIndex = 2;
+            this.txtPassword.Location = new System.Drawing.Point(320, 204);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(183, 20);
+            this.txtPassword.TabIndex = 2;
             // 
-            // label1
+            // lblNameLogin
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(106, 135);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 20);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Tên đăng nhập";
+            this.lblNameLogin.AutoSize = true;
+            this.lblNameLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNameLogin.Location = new System.Drawing.Point(185, 147);
+            this.lblNameLogin.Name = "lblNameLogin";
+            this.lblNameLogin.Size = new System.Drawing.Size(129, 20);
+            this.lblNameLogin.TabIndex = 0;
+            this.lblNameLogin.Text = "Tên đăng nhập";
             // 
-            // label2
+            // lblPassword
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(141, 200);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 20);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Mật khẩu";
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(185, 204);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(83, 20);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Mật khẩu";
+            this.lblPassword.Click += new System.EventHandler(this.label2_Click);
             // 
-            // button1
+            // bttLogin
             // 
-            this.button1.Location = new System.Drawing.Point(183, 286);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 26);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.bttLogin.Location = new System.Drawing.Point(243, 279);
+            this.bttLogin.Name = "bttLogin";
+            this.bttLogin.Size = new System.Drawing.Size(92, 28);
+            this.bttLogin.TabIndex = 3;
+            this.bttLogin.Text = "Đăng nhập";
+            this.bttLogin.UseVisualStyleBackColor = true;
+            this.bttLogin.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // bttExit
             // 
-            this.button2.Location = new System.Drawing.Point(349, 286);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(64, 28);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.bttExit.Location = new System.Drawing.Point(411, 279);
+            this.bttExit.Name = "bttExit";
+            this.bttExit.Size = new System.Drawing.Size(92, 28);
+            this.bttExit.TabIndex = 4;
+            this.bttExit.Text = "Thoát";
+            this.bttExit.UseVisualStyleBackColor = true;
+            this.bttExit.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(714, 422);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bttExit);
+            this.Controls.Add(this.bttLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblNameLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtNameLogin);
             this.Controls.Add(this.LBDangnhap);
             this.Name = "Form1";
-            this.Text = "Đang nhập";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "3";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -124,12 +127,12 @@
         #endregion
 
         private System.Windows.Forms.Label LBDangnhap;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtNameLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblNameLogin;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button bttLogin;
+        private System.Windows.Forms.Button bttExit;
     }
 }
 
--- Hotel Management/Form1.cs
+++ Hotel Management/Form1.cs
@@ -31,5 +31,15 @@ namespace Hotel_Management
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
