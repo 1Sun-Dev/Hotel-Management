2db944b9d0b9eb9126564ee050e7d54cc70d8d6c
Nhi_Tang <tanghotuyetnhi05052005@gmail.com>
2025-10-19 21:00:25 +0700

    commit

--- Hotel Management/Connection.cs
+++ Hotel Management/Connection.cs
@@ -8,10 +8,10 @@ namespace Hotel_Management
 {
      class Connection
     {
-        private string stringConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\USERS\ADMIN\SOURCE\REPOS\HOTEL MANAGEMENT\HOTEL MANAGEMENT\Database1.mdf;Integrated Security=True;Connect Timeout=30";
-        public SqlConnection GetSqlConnection()
-        {
-            return new SqlConnection(stringConnection);
-        }
+        //private string stringConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\USERS\ADMIN\SOURCE\REPOS\HOTEL MANAGEMENT\HOTEL MANAGEMENT\Database1.mdf;Integrated Security=True;Connect Timeout=30";
+        //public SqlConnection GetSqlConnection()
+        //{
+        //    return new SqlConnection(stringConnection);
+        //}
     }
 }
